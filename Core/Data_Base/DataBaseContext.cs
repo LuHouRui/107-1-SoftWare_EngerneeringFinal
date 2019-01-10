c827cf85c34017638ad17ec3bfd285f81290c802
厚銳 呂 <drought0621@Gmail.com>
2019-01-10 18:02:09 +0800

    add

---
+++ Core/Data_Base/DataBaseContext.cs
@@ -0,0 +1,29 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data_Base
+{
+    class DataBaseContext:DbContext
+    {
+        public string ConnenctionString
+        {
+            get
+            {
+                return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\DcTenXen0621\Data\School\107-1-SoftWare_Engerneering\MyWeb\Core\App_Data\MyDataBase.mdf;Integrated Security=True";
+            }
+        }
+        public DbSet<Customer> Customer { get; set; }
+
+        public DataBaseContext() : base() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnenctionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
